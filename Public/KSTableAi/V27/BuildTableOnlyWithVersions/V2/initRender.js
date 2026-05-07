b2233e9d349382bca2b14db50ae44e2dcc36db85
keshavsoft <nknnkumar@yahoo.com>
2026-05-07 13:54:31 +0530

    kstable v27 more stable

--- Public/KSTableAi/V27/BuildTableOnlyWithVersions/V2/initRender.js
+++ Public/KSTableAi/V27/BuildTableOnlyWithVersions/V2/initRender.js
@@ -5,54 +5,94 @@ import { buildDataLists } from "./BuildDataLists/addToDom.js";
 import { hookAllListeners } from "./AddListeners/start.js";
 import { afterMutation } from "./afterMutation.js";
 
-const initRender = ({ inContainerEl, inDataStore, inDom, inServices, inOptions, inEndPoints,
-    inColumnsConfig, inUiClasses, callbacks, inConfig, inToSaveRow }) => {
+const prepareData = ({ inDataStore, inOptions }) => {
+    return {
+        data: inDataStore.getData(),
+        columns: inDataStore.getColumns(),
+        visibleColumns: inDataStore.getVisibleColumns(),
+        visibleColumnsConfig: inDataStore.getVisibleColumnsConfig(),
 
-    const data = inDataStore.getData();
-    const columns = inDataStore.getColumns();
-    const visibleColumns = inDataStore.getVisibleColumns();
-    const visibleColumnsConfig = inDataStore.getVisibleColumnsConfig();
-    // debugger
-    const defaultRow = inDataStore.getDefaultRow();
-    const toSaveRow = inDataStore.getToSaveRow();
+        defaultRow: inDataStore.getDefaultRow(),
+        toSaveRow: inDataStore.getToSaveRow(),
 
-    const isBuildDataLists = inOptions.dataList.show;
-    const showActions = inOptions.table.showRowOptions;
-    const showSerial = inOptions.table.showSerial;
-    const showTable = inOptions.table.showTable;
+        isBuildDataLists: inOptions.dataList.show,
+        showActions: inOptions.table.showRowOptions,
+        showSerial: inOptions.table.showSerial,
+        showTable: inOptions.table.showTable
+    };
+};
 
-    const handleDelete = async ({ presentPk }) => {
-        // debugger;
+const buildDeleteHandler = ({
+    handleDelete,
+    inServices,
+    inEndPoints,
+    inConfig,
+    inDataStore,
+    inContainerEl,
+    inDom,
+    inOptions,
+    visibleColumns,
+    showActions,
+    showSerial,
+    showTable,
+    isBuildDataLists
+}) => {
+    return async ({ presentPk }) => {
         const fromDelete = await inServices.actions.table.delete({
             inEndPoint: inEndPoints.delete,
             id: presentPk
-        })
+        });
 
         if (fromDelete.ok) {
             if (inConfig?.callbacks?.table?.onDelete) {
-                const fromClient = await inConfig?.callbacks?.table?.onDelete({ toDeletePk: presentPk });
+                const fromClient = await inConfig.callbacks.table.onDelete({
+                    toDeletePk: presentPk
+                });
 
                 if (fromClient.ok) {
                     afterMutation({
-                        inServices, inEndPoints, inDataStore, inContainerEl,
-                        inDom, inOptions, inVisibleColumns: visibleColumns,
-                        onDelete: handleDelete, isBuildDataLists,
+                        inServices,
+                        inEndPoints,
+                        inDataStore,
+                        inContainerEl,
+                        inDom,
+                        inOptions,
+                        inVisibleColumns: visibleColumns,
+                        onDelete: handleDelete,
+                        isBuildDataLists,
                         inShowActions: showActions,
-                        inShowSerial: showSerial, inShowTable: showTable
+                        inShowSerial: showSerial,
+                        inShowTable: showTable
                     });
                 };
             } else {
                 afterMutation({
-                    inServices, inEndPoints, inDataStore, inContainerEl,
-                    inDom, inOptions, inVisibleColumns: visibleColumns,
-                    onDelete: handleDelete, isBuildDataLists,
+                    inServices,
+                    inEndPoints,
+                    inDataStore,
+                    inContainerEl,
+                    inDom,
+                    inOptions,
+                    inVisibleColumns: visibleColumns,
+                    onDelete: handleDelete,
+                    isBuildDataLists,
                     inShowActions: showActions,
-                    inShowSerial: showSerial, inShowTable: showTable
+                    inShowSerial: showSerial,
+                    inShowTable: showTable
                 });
             };
         };
     };
-    // debugger;
+};
+
+const renderDataLists = ({
+    isBuildDataLists,
+    inContainerEl,
+    inDataStore,
+    inDom,
+    visibleColumns,
+    data
+}) => {
     if (isBuildDataLists) {
         buildDataLists({
             inContainerEl,
@@ -62,7 +102,14 @@ const initRender = ({ inContainerEl, inDataStore, inDom, inServices, inOptions, 
             inData: data
         });
     };
+};
 
+const renderSearchRow = ({
+    inOptions,
+    inContainerEl,
+    inDataStore,
+    inDom
+}) => {
     if (inOptions.firstRow.showSearch) {
         renderTableHeading({
             inContainerEl,
@@ -74,7 +121,26 @@ const initRender = ({ inContainerEl, inDataStore, inDom, inServices, inOptions, 
 
         firstRow.style.display = "none";
     };
+};
 
+const renderTable = ({
+    showTable,
+    inOptions,
+    inContainerEl,
+    inDataStore,
+    inDom,
+    inServices,
+    inEndPoints,
+    inColumnsConfig,
+    data,
+    visibleColumns,
+    handleDelete,
+    showActions,
+    showSerial,
+    defaultRow,
+    inUiClasses,
+    toSaveRow
+}) => {
     if (showTable) {
         const inIsDisabled = inOptions.table.isDisabled;
 
@@ -99,8 +165,99 @@ const initRender = ({ inContainerEl, inDataStore, inDom, inServices, inOptions, 
             inToSaveRow: toSaveRow
         });
     };
+};
 
-    hookAllListeners({ inContainerEl, inDataStore, inDom, inOptions });
+const hookDomListeners = ({
+    inContainerEl,
+    inDataStore,
+    inDom,
+    inOptions
+}) => {
+    hookAllListeners({
+        inContainerEl,
+        inDataStore,
+        inDom,
+        inOptions
+    });
+};
+
+const initRender = ({ inContainerEl, inDataStore, inDom, inServices, inOptions, inEndPoints,
+    inColumnsConfig, inUiClasses, callbacks, inConfig, inToSaveRow }) => {
+
+    const localData = prepareData({
+        inDataStore,
+        inOptions
+    });
+
+    const {
+        data,
+        columns,
+        visibleColumns,
+        visibleColumnsConfig,
+        defaultRow,
+        toSaveRow,
+        isBuildDataLists,
+        showActions,
+        showSerial,
+        showTable
+    } = localData;
+
+    const handleDelete = buildDeleteHandler({
+        inServices,
+        inEndPoints,
+        inConfig,
+        inDataStore,
+        inContainerEl,
+        inDom,
+        inOptions,
+        visibleColumns,
+        showActions,
+        showSerial,
+        showTable,
+        isBuildDataLists
+    });
+
+    renderDataLists({
+        isBuildDataLists,
+        inContainerEl,
+        inDataStore,
+        inDom,
+        visibleColumns,
+        data
+    });
+
+    renderSearchRow({
+        inOptions,
+        inContainerEl,
+        inDataStore,
+        inDom
+    });
+
+    renderTable({
+        showTable,
+        inOptions,
+        inContainerEl,
+        inDataStore,
+        inDom,
+        inServices,
+        inEndPoints,
+        inColumnsConfig,
+        data,
+        visibleColumns,
+        handleDelete,
+        showActions,
+        showSerial,
+        defaultRow,
+        inUiClasses,
+        toSaveRow
+    });
+
+    hookDomListeners({
+        inContainerEl,
+        inDataStore,
+        inDom,
+        inOptions
+    });
 };
 
 export { initRender };
